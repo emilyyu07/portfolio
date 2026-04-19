30a9a5bc655c2c630421409b6e72b05e10d2c058
Emily Yu <emilyyu60@gmail.com>
2026-04-19 19:44:20 -0400

    fixed site margins

--- src/components/misc/AirpodsHoverTrigger.tsx
+++ src/components/misc/AirpodsHoverTrigger.tsx
@@ -3,13 +3,40 @@
 import Image from "next/image";
 import MusicCard from "@/components/misc/MusicCard";
 import type { Song } from "@/components/misc/SongDisplay";
-import { spotifySongs } from "@/lib/config";
 
-const songs: Song[] = spotifySongs.map((song) => ({
-  title: song.title,
-  artist: song.artist,
-  cover: song.imageUrl,
-}));
+const songs: Song[] = [
+  { title: "I've Seen It", artist: "Olivia Dean", cover: "/songs/1.jpg" },
+  { title: "Call Your Mom", artist: "Noah Kahan", cover: "/songs/2.jpg" },
+  { title: "Come Back to Earth", artist: "Mac Miller", cover: "/songs/3.jpg" },
+  { title: "Free Now", artist: "Gracie Abrams", cover: "/songs/4.jpg" },
+  { title: "Peter", artist: "Taylor Swift", cover: "/songs/5.jpg" },
+  { title: "Loose", artist: "Daniel Caesar", cover: "/songs/6.jpg" },
+  {
+    title: "exile (feat. Bon Iver)",
+    artist: "Taylor Swift, Bon Iver",
+    cover: "/songs/7.jpg",
+  },
+  {
+    title: "Do I Wanna Know?",
+    artist: "Hozier",
+    cover: "/songs/8.jpg",
+  },
+  {
+    title: "All Too Well (Taylor's Version)",
+    artist: "Taylor Swift",
+    cover: "/songs/8.jpg",
+  },
+  {
+    title: "Everywhere, Everything",
+    artist: "Noah Kahan, Gracie Abrams",
+    cover: "/songs/8.jpg",
+  },
+  {
+    title: "No. 1 Party Anthem",
+    artist: "Arctic Monkeys",
+    cover: "/songs/8.jpg",
+  },
+];
 
 type AirpodsHoverTriggerProps = {
   airpodsSrc: string;
